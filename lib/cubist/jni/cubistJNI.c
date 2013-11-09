0f15ef571082442ceda0401a5432ba9e3fed783a
Diego Didona <didona@gsd.inesc-id.pt>
2013-11-09 15:34:35 +0000

    * Avoiding runtime glibc problems

--- lib/cubist/jni/cubistJNI.c
+++ lib/cubist/jni/cubistJNI.c
@@ -28,6 +28,7 @@ JNIEXPORT void JNICALL Java_oracle_CubistOracle_jni_JniCubistOracle_initiateCubi
 
 JNIEXPORT void JNICALL Java_oracle_CubistOracle_jni_JniCubistOracle_deallocLastModel(JNIEnv *env, jobject thisobject){
   FreeCttee(CubistModel);
+    fclose(F);
 }
 
 
